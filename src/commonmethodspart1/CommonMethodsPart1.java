a5b29cd22bb463d2eb4438eae41dd9245d2228b6
hcheraghpoor <h.cheraghpoor@gmail.com>
2017-05-04 15:33:49 +0430

    توابع پر کاربرد ریاضی

---
+++ src/commonmethodspart1/CommonMethodsPart1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package commonmethodspart1;
+
+/**
+ *
+ * @author mohammadHossein
+ */
+public class CommonMethodsPart1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //@@@@@@@@@@@@@@@@@@توابع مثلثاتی
+        Math.sin(Math.PI);
+        Math.cos(0);
+        Math.tan(1);
+        //@@@@@@@@@@@@@@@@@@@
+        Math.abs(-12);//محاسبه قدر مطلق
+        Math.pow(2, 3);//محاسبه توان
+        Math.floor(3.6);//محاسبه کف یک عدد
+        Math.ceil(4.7);//محاسبه سقف یک عدد
+        Math.max(12, 12.90);
+        Math.min(11, -128);
+        Math.sqrt(4);//محاسبه ریشه دوم یک عدد
+        Math.log10(100);
+        
+        Math.random();//تولید عدد تصادفی بین صفر و یک
+        //@@@@@@@@@@@@@@@@@@@@
+        Integer.parseInt("1213");//تبدیل یک رشته به صورت عدد صحیح
+        Float.parseFloat("123.987");
+        Double.parseDouble("-12397.1375");
+        
+        
+    }
+    
+}
